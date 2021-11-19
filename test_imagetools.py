963d12f1bc449c0969ae480c78ae981468621916
williamnoonan <willnoonan@outlook.com>
2021-11-19 12:20:19 -0600

    Added setUpClass to unit test.

--- test_imagetools.py
+++ test_imagetools.py
@@ -3,11 +3,20 @@ from testingtools import blurimageby
 from imagetools import *
 
 class MyTestCase(unittest.TestCase):
-    def test_algorithms_equal(self):
-        v1 = blurimageby(simple_boxblur)["red"].result()
-        v2 = blurimageby(simple_boxblur_V2)["red"].result()
-        self.assertEqual(v1, v2)  # add assertion here
+    @classmethod
+    def setUpClass(cls):
+        """
+        Put variables in setUpClass that you want executed only once and available to all tests.
+        """
+        cls.v1 = blurimageby(simple_boxblur)["red"].result()
 
+    def test_algorithms_equal(self):
+        v2 = blurimageby(boxblur, radius=1)["red"].result()
+        self.assertEqual(self.v1, v2)  # add assertion here
+
+    def test_algorithms_notequal(self):
+        v2 = blurimageby(boxblur, radius=2)["red"].result()
+        self.assertNotEqual(self.v1, v2)
 
 if __name__ == '__main__':
     unittest.main()

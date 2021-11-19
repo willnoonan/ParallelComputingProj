bd8210efe245bbbbf55752885f30be3f723fe6df
williamnoonan <willnoonan@outlook.com>
2021-11-19 11:43:51 -0600

    Created unit test to test different box blur algorithms.

---
+++ test_imagetools.py
@@ -0,0 +1,13 @@
+import unittest
+from test_tools import blurimageby
+from imagetools import *
+
+class MyTestCase(unittest.TestCase):
+    def test_algorithms_equal(self):
+        v1 = blurimageby(simple_boxblur)["red"].result()
+        v2 = blurimageby(simple_boxblur_V2)["red"].result()
+        self.assertEqual(v1, v2)  # add assertion here
+
+
+if __name__ == '__main__':
+    unittest.main()
